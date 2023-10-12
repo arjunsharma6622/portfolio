a22385bcf0faf879b7771f359c4983c66c7e87dd
Arjun Sharma <arjunsharma6622@gmail.com>
2023-10-12 10:51:24 +0530

    SVG images path changed and images dir changed

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -6,19 +6,19 @@ const SocialMedia = () => {
 
       <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer" className="text-accent1 hover:text-accent2 transition duration-300">
         {/* <i className="fab fa-linkedin-in text-3xl"></i> */}
-        <img src="../Images/Icons/Social/linkedin.svg" alt="" className='h-8 w-8'/>
+        <img src="./public/images/Icons/Social/linkedin.svg" alt="" className='h-8 w-8'/>
       </a>
       <a href="https://github.com/" target="_blank" rel="noopener noreferrer" className="text-accent1 hover:text-accent2 transition duration-300">
         {/* <i className="fab fa-github text-3xl"></i> */}
-        <img src="../Images/Icons/Social/github.svg" alt="" className='h-8 w-8'/>
+        <img src="./public/images/Icons/Social/github.svg" alt="" className='h-8 w-8'/>
       </a>
       <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer" className="text-accent1 hover:text-accent2 transition duration-300">
         {/* <i className="fab fa-twitter text-3xl"></i> */}
-        <img src="../Images/Icons/Social/youtube.svg" alt="" className='h-8 w-8'/>
+        <img src="./public/images/Icons/Social/youtube.svg" alt="" className='h-8 w-8'/>
       </a>
       <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer" className="text-accent1 hover:text-accent2 transition duration-300">
         {/* <i className="fab fa-twitter text-3xl"></i> */}
-        <img src="../Images/Icons/Social/medium.svg" alt="" className='h-8 w-8'/>
+        <img src="./public/images/Icons/Social/medium.svg" alt="" className='h-8 w-8'/>
       </a>
       {/* Add more social media icons here */}
     </div>

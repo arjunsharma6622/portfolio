fcc97cd90232afc47a4646d6a59b92af4ca64a04
Arjun Sharma <arjunsharma6622@gmail.com>
2023-10-12 11:32:58 +0530

    SVG images path changed and images dir changed

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -6,7 +6,7 @@ const SocialMedia = () => {
     <div className="fixed -right-1 top-1/2 transform -translate-y-1/2 flex flex-col items-center gap-4 bg-secondary border border-primary rounded-xl px-3 py-4">
 
       <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer" className="text-accent1 hover:text-accent2 transition duration-300">
-        <img src="./images/Icons/Social/linkedin.svg" alt="" className='h-8 w-8'/>
+        <img src="/src/assets/images/Icons/Social/linkedin.svg" alt="" className='h-8 w-8'/>
       </a>
       <a href="https://github.com/" target="_blank" rel="noopener noreferrer" className="text-accent1 hover:text-accent2 transition duration-300">
         <img src="./images/Icons/Social/github.svg" alt="" className='h-8 w-8'/>

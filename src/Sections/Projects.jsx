ea5a199962137f1c4b0bfcc0cae7ebd5fcf21a54
Arjun Sharma <arjunsharma6622@gmail.com>
2023-10-13 18:10:40 +0530

    projects section slider added, and data change

--- src/Sections/Projects.jsx
+++ src/Sections/Projects.jsx
@@ -1,41 +1,96 @@
 import React from 'react';
 import Heading from '../components/Heading';
 import { projectsData } from '../data';
+import Slider from 'react-slick';
+
+
+
+// function ProjectCard({ project }) {
+//   return (
+//     <div className="bg-secondary border border-neutral-700 rounded-xl shadow-lg p-6 ">
+
+//       <h3 className="text-2xl font-semibold mb-4">{project.title}</h3>
+//       <div className='h-60 mb-4'>
+//         <img src={project.image} alt="" className='h-full w-full object-cover rounded-lg' />
+//       </div>
+//       <p className="text-gray-600 mb-4">{project.description}</p>
+//       <div className="mb-4">
+//         <ul className="text-gray-600 flex flex-wrap text-sm gap-1">
+//           {project.techStack.map((tech, index) => (
+
+//             <li key={index} className='border border-primary rounded-full px-3 py-[1.5px]' >{tech}</li>
+//           ))}
+//         </ul>
+//       </div>
+//       <div className="flex">
+//         <a href={project.demoLink} target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline mr-4 flex justify-start items-center">
+//           <div className='flex justify-center gap-2 items-center'>
+//             <img src="./images/Frame 1.svg" alt="" />
+//             <span>Demo</span>
+//           </div>
+
+//         </a>
+
+//         <a href={project.codeLink} target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline flex justify-start items-center">
+//           <div className='flex justify-center gap-2 items-center'>
+
+//             <img src="./images/Frame 2.svg" alt="" />
+//             <span>Code</span>
+//           </div>
+
+//         </a>
+//       </div>
+//     </div>
+//   );
+// }
+
 
 
 function ProjectCard({ project }) {
+  const settings = {
+    // dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    arrows: false,
+  };
+
   return (
     <div className="bg-secondary border border-neutral-700 rounded-xl shadow-lg p-6 ">
-
       <h3 className="text-2xl font-semibold mb-4">{project.title}</h3>
       <div className='h-60 mb-4'>
-        <img src={project.image} alt="" className='h-full w-full object-cover rounded-lg' />
+        <Slider {...settings}>
+          {project.images.map((image, index) => (
+            <img src={`./images/projects/${project.dir_name}/${image}.png`} alt={`project-${index}`} className='h-full w-full object-contain rounded-lg' key={index} />
+          ))}
+        </Slider>
       </div>
       <p className="text-gray-600 mb-4">{project.description}</p>
       <div className="mb-4">
         <ul className="text-gray-600 flex flex-wrap text-sm gap-1">
           {project.techStack.map((tech, index) => (
-
             <li key={index} className='border border-primary rounded-full px-3 py-[1.5px]' >{tech}</li>
           ))}
         </ul>
       </div>
       <div className="flex">
-        <a href={project.demoLink} target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline mr-4 flex justify-start items-center">
-          <div className='flex justify-center gap-2 items-center'>
-            <img src="./images/Frame 1.svg" alt="" />
-            <span>Demo</span>
-          </div>
-
-        </a>
+        {project.demoLink && (
+          <a href={project.demoLink} target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline mr-4 flex justify-start items-center">
+            <div className='flex justify-center gap-2 items-center'>
+              <img src="./images/Frame 1.svg" alt="" />
+              <span>Demo</span>
+            </div>
+          </a>
+        )}
 
         <a href={project.codeLink} target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline flex justify-start items-center">
           <div className='flex justify-center gap-2 items-center'>
-
             <img src="./images/Frame 2.svg" alt="" />
             <span>Code</span>
           </div>
-
         </a>
       </div>
     </div>
@@ -48,7 +103,7 @@ function Projects() {
 
       <div className="container mx-auto w-8/12">
 
-        <Heading name={'PROJECTS'} heading_desc={'A Portfolio of Projects'}/>
+        <Heading name={'PROJECTS'} heading_desc={'A Portfolio of Projects'} />
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
           {projectsData.map((project, index) => (
--- src/data.js
+++ src/data.js
@@ -1,114 +1,99 @@
 export const socialLinks = [
-    {
-        icon : './images/Icons/Social/Linkedin.svg',
-        link : 'https://www.linkedin.com/in/arjunsharma6622/'
-    },
-    {
-        icon : './images/Icons/Social/github.svg',
-        link : 'https://github.com/Arjun27sharma'
-    },
-    {
-        icon : './images/Icons/Social/Leetcode.svg',
-        link : 'https://leetcode.com/arjunsharma6622/'
-    },
-    {
-        icon : './images/Icons/Social/Medium.svg',
-        link : 'https://medium.com/@arjunsharma6622'
-    },
-    {
-        icon : './images/Icons/Social/Youtube.svg',
-        link : 'https://www.youtube.com/channel/UCZnUTPEaqhEEyWAehyVwoCA'
-    },
+  {
+    icon: './images/Icons/Social/Linkedin.svg',
+    link: 'https://www.linkedin.com/in/arjunsharma6622/'
+  },
+  {
+    icon: './images/Icons/Social/github.svg',
+    link: 'https://github.com/Arjun27sharma'
+  },
+  {
+    icon: './images/Icons/Social/Leetcode.svg',
+    link: 'https://leetcode.com/arjunsharma6622/'
+  },
+  {
+    icon: './images/Icons/Social/Medium.svg',
+    link: 'https://medium.com/@arjunsharma6622'
+  },
+  {
+    icon: './images/Icons/Social/Youtube.svg',
+    link: 'https://www.youtube.com/channel/UCZnUTPEaqhEEyWAehyVwoCA'
+  },
 ]
 
 
 export const skillsData = [
-    { name: 'JavaScript', imageSrc: './images/Icons/js.svg', tag: 'Web' },
-    { name: 'Python', imageSrc: './images/Icons/python.svg', tag: 'Web' },
-    { name: 'C++', imageSrc: './images/Icons/c++.svg', tag: 'Web' },
-    { name: 'HTML', imageSrc: './images/Icons/html5.svg', tag: 'Web' },
-    { name: 'CSS', imageSrc: './images/Icons/css.svg', tag: 'Web' },
-  
-    { name: 'React', imageSrc: './images/Icons/reactjs.svg', tag: 'Web' },
-    { name: 'Redux', imageSrc: './images/Icons/redux.svg', tag: 'Web' },
-    { name: 'Tailwind', imageSrc: './images/Icons/tailwind.svg', tag: 'Web' },
-    { name: 'SASS', imageSrc: './images/Icons/sass.svg', tag: 'Web' },
-    { name: 'Bootstrap', imageSrc: './images/Icons/bootstrap5.svg', tag: 'Web' },
-    { name: 'Material UI', imageSrc: './images/Icons/material-ui.svg', tag: 'Web' },
-    { name: 'Figma', imageSrc: './images/Icons/figma.svg', tag: 'Web' },
-    { name: 'Canva', imageSrc: './images/Icons/canva.svg', tag: 'Web' },
-  
-    { name: 'NodeJs', imageSrc: './images/Icons/nodejs.svg', tag: 'Web' },
-    { name: 'MongoDb', imageSrc: './images/Icons/mongodb.svg', tag: 'Web' },
-    { name: 'Postman', imageSrc: './images/Icons/postman.svg', tag: 'Web' },
-    { name: 'Firebase', imageSrc: './images/Icons/firebase.svg', tag: 'Web' },
-    { name: 'npm', imageSrc: './images/Icons/npm.svg', tag: 'Web' },
-    { name: 'json', imageSrc: './images/Icons/json.svg', tag: 'Web' },
-    { name: 'VS Code', imageSrc: './images/Icons/vs.svg', tag: 'Web' },
-    { name: 'Git', imageSrc: './images/Icons/git.svg', tag: 'Web' },
-    { name: 'GitHub', imageSrc: './images/Icons/github.svg', tag: 'Web' },
-    { name: 'OpenAI', imageSrc: './images/Icons/openai.svg', tag: 'Web' },
-  ];
+  { name: 'JavaScript', imageSrc: './images/Icons/js.svg', tag: 'Web' },
+  { name: 'Python', imageSrc: './images/Icons/python.svg', tag: 'Web' },
+  { name: 'C++', imageSrc: './images/Icons/c++.svg', tag: 'Web' },
+  { name: 'HTML', imageSrc: './images/Icons/html5.svg', tag: 'Web' },
+  { name: 'CSS', imageSrc: './images/Icons/css.svg', tag: 'Web' },
+
+  { name: 'React', imageSrc: './images/Icons/reactjs.svg', tag: 'Web' },
+  { name: 'Redux', imageSrc: './images/Icons/redux.svg', tag: 'Web' },
+  { name: 'Tailwind', imageSrc: './images/Icons/tailwind.svg', tag: 'Web' },
+  { name: 'SASS', imageSrc: './images/Icons/sass.svg', tag: 'Web' },
+  { name: 'Bootstrap', imageSrc: './images/Icons/bootstrap5.svg', tag: 'Web' },
+  { name: 'Material UI', imageSrc: './images/Icons/material-ui.svg', tag: 'Web' },
+  { name: 'Figma', imageSrc: './images/Icons/figma.svg', tag: 'Web' },
+  { name: 'Canva', imageSrc: './images/Icons/canva.svg', tag: 'Web' },
+
+  { name: 'NodeJs', imageSrc: './images/Icons/nodejs.svg', tag: 'Web' },
+  { name: 'MongoDb', imageSrc: './images/Icons/mongodb.svg', tag: 'Web' },
+  { name: 'Postman', imageSrc: './images/Icons/postman.svg', tag: 'Web' },
+  { name: 'Firebase', imageSrc: './images/Icons/firebase.svg', tag: 'Web' },
+  { name: 'npm', imageSrc: './images/Icons/npm.svg', tag: 'Web' },
+  { name: 'json', imageSrc: './images/Icons/json.svg', tag: 'Web' },
+  { name: 'VS Code', imageSrc: './images/Icons/vs.svg', tag: 'Web' },
+  { name: 'Git', imageSrc: './images/Icons/git.svg', tag: 'Web' },
+  { name: 'GitHub', imageSrc: './images/Icons/github.svg', tag: 'Web' },
+  { name: 'OpenAI', imageSrc: './images/Icons/openai.svg', tag: 'Web' },
+];
 
 
 
 export const projectsData = [
-    {
-      title: 'Netflix Clone',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      techStack: ['React', 'Node.js', 'Express', 'MongoDB'],
-      demoLink: 'https://example.com/demo1',
-      codeLink: 'https://github.com/yourusername/project1',
-      image: 'https://miro.medium.com/v2/resize:fit:2000/1*hLrWUW2mKy-GTeujcXl0NA.png'
-    },
-    {
-      title: 'Serivce listing website',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      techStack: ['React', 'Node.js', 'Express', 'MongoDB', 'Tailwindcss'],
-      demoLink: 'https://example.com/demo1',
-      codeLink: 'https://github.com/yourusername/project1',
-      image: 'https://9to5mac.com/wp-content/uploads/sites/6/2019/07/twitter-desktop-dark-mode-new-design.jpeg?quality=82&strip=all'
-    },
-    {
-      title: 'Ecommerce Website',
-      description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.',
-      techStack: ['React', 'Redux', 'Firebase'],
-      demoLink: 'https://example.com/demo2',
-      codeLink: 'https://github.com/yourusername/project2',
-      image:'https://websitesetup.org/wp-content/uploads/2023/02/WooCommerce-ecommerce-platform-February-2023.jpg'
-    },
-    {
-      title: 'Facebook Clone',
-      description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.',
-      techStack: ['React', 'Redux', 'Firebase'],
-      demoLink: 'https://example.com/demo2',
-      codeLink: 'https://github.com/yourusername/project2',
-      image:'https://engineering.fb.com/wp-content/uploads/2020/05/2.-Home-Setting-Dark-Mode.png'
-    },
-    {
-      title: 'Form Fix',
-      description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.',
-      techStack: ['React', 'Redux', 'Firebase'],
-      demoLink: 'https://example.com/demo2',
-      codeLink: 'https://github.com/yourusername/project2',
-      image:'https://engineering.fb.com/wp-content/uploads/2020/05/2.-Home-Setting-Dark-Mode.png'
-    },
-  ];
+  {
+    title: 'Ecommerce Website',
+    description: 'Engineered a comprehensive Ecommerce platform using React, MongoDB, Node.js, and Bootstrap, providing users with a seamless and secure shopping experience. Implemented secure payment processing with Stripe, ensuring hassle-free transactions and enhancing user trust. Created an intuitive and responsive interface for effortless navigation and product exploration.',
+    techStack: ['React', 'Redux', 'Firebase'],
+    demoLink: 'https://ecommerce-64e9b.web.app/',
+    codeLink: 'https://github.com/Arjun27sharma/CCC_Ecommerce',
+    images: [1, 2, 3, 4, 5, 6],
+    dir_name : 'ecommerce'
+  },
+  {
+    title: 'Netflix Clone',
+    description: 'Developed an immersive Netflix-inspired web application using React, MongoDB, Node.js, and SASS, facilitating dynamic content delivery. Employed Firebase for real-time data storage, ensuring smooth and seamless user experiences. Integrated various user-friendly features to enhance engagement and interaction.',
+    techStack: ['React', 'Node.js', 'Express', 'MongoDB'],
+    codeLink: 'https://github.com/Arjun27sharma/Netflix-Clone',
+    images: [1, 2, 3, 4, 5, 6, 7],
+    dir_name : 'netflix'
+  },
+  {
+    title: 'Social Web App',
+    description: 'Built a dynamic and engaging social media application using React and Node.js, enabling live messaging, interactive posts, likes, and follows for enhanced user engagement. Integrated a robust Node.js-MongoDB backend with Multer for efficient multimedia uploads, providing users with a seamless content sharing experience. Prioritized user interaction and feedback to create a vibrant and active community.',
+    techStack: ['React', 'Node.js', 'Express', 'MongoDB', 'Tailwindcss'],
+    codeLink: 'https://github.com/Arjun27sharma/socialmedia',
+    images: [1, 2, 3, 4, 5],
+    dir_name : 'socialapp'
+  },
+];
 
 export const experienceData = [
-    {
-      title: 'Web Development Intern',
-      company: 'Aresuno',
-      date: 'May 2023 – Jul 2023',
-      description: 'Developed the frontend of a sanitary ecommerce website using HTML, CSS, Bootstrap, and JavaScript. Collaborated with the team to enhance user experience and implement interactive features.',
-      icon:'./images/GalaxyGrow.png'
-    },
-    {
-      title: 'Freelance Web Developer',
-      company: 'Self-Employed',
-      date: 'May 2023 – Present',
-      description: 'Engineered a real-time exercise form evaluation app employing WebRTC, Socket.IO, and OpenCV technologies. It provided instant feedback and guidance for users\' workout techniques, enhancing safety and effectiveness.',
-      icon:'./images/GalaxyGrow.png'
-    },
-    // Add more work experiences here
-  ];
+  {
+    title: 'Web Development Intern',
+    company: 'Aresuno',
+    date: 'May 2023 – Jul 2023',
+    description: 'Developed the frontend of a sanitary ecommerce website using HTML, CSS, Bootstrap, and JavaScript. Collaborated with the team to enhance user experience and implement interactive features.',
+    icon: './images/GalaxyGrow.png'
+  },
+  {
+    title: 'Freelance Web Developer',
+    company: 'Self-Employed',
+    date: 'May 2023 – Present',
+    description: 'Engineered a real-time exercise form evaluation app employing WebRTC, Socket.IO, and OpenCV technologies. It provided instant feedback and guidance for users\' workout techniques, enhancing safety and effectiveness.',
+    icon: './images/GalaxyGrow.png'
+  },
+  // Add more work experiences here
+];

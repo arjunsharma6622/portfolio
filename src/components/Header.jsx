0a100f63dc5cdd4e90e42463696584dc919e06bc
Arjun Sharma <arjunsharma6622@gmail.com>
2024-02-06 13:31:13 +0530

    uploaded resume

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,17 +12,18 @@ function Header() {
 
   
   return (
-    <header className=" text-white px-16 py-8 relative" id='home'>
+    <div>
+    <header className=" text-white py-8 relative" id='home'>
               <div className="w-96 h-96 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
           <div className=" w-96 h-48 absolute bg-blue-600 bg-opacity-50 rounded-full blur-3xl" />
           <div className=" w-96 h-48 left-64 absolute bg-blue-600 bg-opacity-50 rounded-full blur-3xl" />
           <div className=" w-96 h-48 right-64 absolute bg-blue-600 bg-opacity-50 rounded-full blur-3xl" />
         </div>
-        <div className="container mx-auto flex justify-between items-center w-8/12 px-4 py-4 rounded-xl" style={{ background: 'rgba(26, 26, 26, 0.483)', backdropFilter: 'blur(0.4px)' }}>
+        <div className="container mx-auto flex justify-between items-center lg:w-8/12 md:1/2 max-[740px]:w-[100%] max-[740px]:flex-col max-[740px]:gap-4 max-[370px]:w-[100%] px-4 py-4 rounded-xl" style={{ background: 'rgba(26, 26, 26, 0.483)', backdropFilter: 'blur(0.4px)' }}>
         <div>
           <h1 className="text-2xl font-bold">🙂 Arjun Sharma</h1>
         </div>
-        <nav className="space-x-4">
+        <nav className="space-x-4 max-[370px]:hidden">
           <a href="#about" className=" hover:text-accent2 transition duration-300">About</a>
           <a href="#skills" className=" hover:text-accent2 transition duration-300">Skills</a>
           <a href="#experience" className=" hover:text-accent2 transition duration-300">Experience</a>
@@ -31,6 +32,10 @@ function Header() {
         </nav>
       </div>
     </header>
+
+
+
+    </div>
   );
 }
 
